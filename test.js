e23f518611d1e4f573bc56ac8eb2b984da84e6fe
CesariaMonforte <cmonforte@my.bcit.ca>
2024-09-16 10:30:48 -0800

    Lab 2

---
+++ test.js
@@ -0,0 +1,16 @@
+
+const show="The Flash";
+let Platform="Netflix";
+let leisureHours= 3;
+let academicHours= 4;
+
+
+
+console.log("To pass time during the week I usually watch shows on "+Platform+" or do some school working the library. I watch new "+Platform+" shows.My favourite show is The flash I watch "+Platform+" because there's always something", "\x1b[32m","new","\x1b[0m"," and interesting such as a new novel or just watching something i watched a long time ago. I spend probably about "+leisureHours+" hours a day during the week on "+Platform+" and "+academicHours+" hours a day studying."); //You can check the spaces between words and periods. Also, really great color use. And I understand that you need to break the paragraph in two, and just then change the variable in the secound paragraph. And the variable could be something else then a number, the number could ne added just like +3+ and +4+. - Bruna
+
+
+Platform="Hulu";
+leisureHours=10;
+academicHours=2;
+
+console.log(`To pass time during the week I usually watch shows on ${Platform} or do some school working the library. I watch new ${Platform} shows.My favourite show is The flash I watch ${Platform} because there's always something new and interesting such as a new novel or just watching something i watched a long time ago. I spend probably about ${leisureHours} hours a day during the week on ${Platform} and ${academicHours} hours a day studying.`); // you could practice using ${____} in your second paragraph. So intead of using ("To pass time..."), you could replace " with a back tick ` (~ key) and then type your variables as ${____}. -- kunwar
